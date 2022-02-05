f6c141f0b6d0a6b53bcc0e63fad6cecffadfe926
nwankpa-D-boy <nwankpakelechisamuel@gmail.com>
2022-02-05 19:26:23 +0000

    Updated files on the 5th Feb, 2022

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -1,8 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import RegisterForm from "../components/RegisterForm";
+import axios from 'axios';
+
 const Register = () => {
+
+    const [userCredentials, setUserCredentials] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const { name, email, password } = userCredentials;
+
+    const handleChange = (event) => {
+        const { value, id } = event.target;
+        setUserCredentials({ ...userCredentials, [id]: value })
+
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.table({ name, email, password})
+
+
+    }
+
+   
     return (
-        <div className="container-fluid h1 p-5 text-center"> 
-            Register Page
-        </div>
+        <>
+
+            <div className="container-fluid bg-secondary h1 p-4 text-center">
+                <h1>Register Page</h1>
+            </div>
+
+            <div className="container">
+                <div className="row">
+                    <div>
+                        <RegisterForm 
+                           handleChange={handleChange}
+                           handleSubmit={handleSubmit}
+                           name={name}
+                           email={email}
+                           password={password}
+                        />
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
---
+++ client/src/components/RegisterForm.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const RegisterForm = ({
+    handleSubmit,
+    handleChange,
+    email,
+    name,
+    password
+
+}) => {
+    
+    return (<form onSubmit={handleSubmit} className="mt-3">
+        <div className="mb-3">
+            <label className="form-label">Name</label>
+            <input
+                id="name"
+                type="text"
+                className="form-control"
+                placeholder="Enter Name"
+                onChange={handleChange}
+                value={name}
+            />
+        </div>
+
+        <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input
+                id="email"
+                type="email"
+                className="form-control"
+                placeholder="Enter Email"
+                onChange={handleChange}
+                value={email}
+            />
+        </div>
+
+        <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+                id="password"
+                type="password"
+                className="form-control"
+                placeholder="Enter Password"
+                onChange={handleChange}
+                value={password}
+            />
+        </div>
+
+        <button className="btn btn-primary">Submit</button>
+    </form>)
+
+}
+
+export default RegisterForm;
