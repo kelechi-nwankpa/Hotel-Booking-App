e0a96adb6878e25d1a7724072503d984fff6ae61
kelechi-nwankpa <nnunnunnu90@gmail.com>
2022-02-23 17:47:14 +0000

    Capitalized to HOME PAGE

--- client/src/booking/Home.js
+++ client/src/booking/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
     return (
         <div className="container-fluid h1 p-5 text-center"> 
-            Home Page {JSON.stringify(user)}
+            HOME PAGE {JSON.stringify(user)}
         </div>
     )
 }
